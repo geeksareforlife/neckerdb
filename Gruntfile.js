3164423dfe41a9e6fff8f2b67ce3e714e98c8819
James Hayward <jhayward1980@gmail.com>
2026-01-23 15:17:12 +0000

    Build search Index MVP

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,11 @@
 // Adapted from https://gist.github.com/sebz/efddfc8fdcb6b480f567
 
 var yaml = require("yamljs");
+var toml = require("toml");
 var S = require("string");
+var lunr = require("lunr");
+
+var allColours = {};
 
 var CONTENT_PATH_PREFIX = "content";
 
@@ -44,10 +48,17 @@ module.exports = function(grunt) {
                 
             });
 
-            grunt.log.debug(JSON.stringify(pagesIndex));
-
+            grunt.log.ok("Document Array Built")
             return pagesIndex;
         };
+
+        var populateAllColours = function() {
+			var config = toml.parse(grunt.file.read("hugo.toml"));
+
+			for (const name in config.params.colours) {
+				allColours[name] = config.params.colours[name].basic;
+			}
+		};
 
         var processFile = function(abspath, filename) {
         	var pageIndex;
@@ -70,12 +81,74 @@ module.exports = function(grunt) {
 
         	pageIndex =  {
         		href: href,
-        		title: frontMatter.title
+        		title: frontMatter.title,
+        		colours: processColours(frontMatter)
         	};
 
         	return pageIndex;
-        }
+        };
 
-        indexPages();
+        var processColours = function(frontMatter) {
+        	var colours = [];
+
+        	if ("main" in frontMatter.params) {
+        		colours.push(...allColours[frontMatter.params.main]);
+        	}
+        	if("outerBorder" in frontMatter.params) {
+        		colours.push(...allColours[frontMatter.params.outerBorder]);
+        	}
+        	if ("middleBorder" in frontMatter.params) {
+        		colours.push(...allColours[frontMatter.params.middleBorder]);
+        	}
+        	if ("innerBorder" in frontMatter.params) {
+        		colours.push(...allColours[frontMatter.params.innerBorder]);
+        	}
+			if ("leftOuterBorder" in frontMatter.params) {
+        		colours.push(...allColours[frontMatter.params.leftOuterBorder]);
+        	}
+			if ("rightOuterBorder" in frontMatter.params) {
+        		colours.push(...allColours[frontMatter.params.rightOuterBorder]);
+        	}
+			if ("leftMiddleBorder" in frontMatter.params) {
+        		colours.push(...allColours[frontMatter.params.leftMiddleBorder]);
+        	}
+			if ("rightMiddleBorder" in frontMatter.params) {
+        		colours.push(...allColours[frontMatter.params.rightMiddleBorder]);
+        	}
+			if ("leftInnerBorder" in frontMatter.params) {
+        		colours.push(...allColours[frontMatter.params.leftInnerBorder]);
+        	}
+			if ("rightInnerBorder" in frontMatter.params) {
+        		colours.push(...allColours[frontMatter.params.rightInnerBorder]);
+        	}
+			if ("leftMain" in frontMatter.params) {
+        		colours.push(...allColours[frontMatter.params.leftMain]);
+        	}
+			if ("rightMain" in frontMatter.params) {
+        		colours.push(...allColours[frontMatter.params.rightMain]);
+        	} 
+			if ("centre" in frontMatter.params) {
+        		colours.push(...allColours[frontMatter.params.centre]);
+        	}
+
+        	return [...new Set(colours)];
+        };
+
+
+
+        populateAllColours();
+        
+        idx = lunr(function() {
+        	this.ref('href');
+        	this.field('title');
+        	this.field('colours');
+
+        	indexPages().forEach(function (doc) {
+        		this.add(doc)
+        	}, this);
+        });
+
+        grunt.file.write("assets/js/lunr/PagesIndex.js", "var idx = lunr.Index.load(JSON.parse(" + JSON.stringify(idx) + "));");
+        grunt.log.ok("Index built");
 	});
 };
